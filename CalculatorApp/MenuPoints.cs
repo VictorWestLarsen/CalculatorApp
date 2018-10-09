6b5da144fec56d7532d3ac92c81a117a759d0156
Christian T <chri466w@edu.eal.dk>
2018-10-09 14:36:42 +0200

    Reimplemented Multiply

--- CalculatorApp/MenuPoints.cs
+++ CalculatorApp/MenuPoints.cs
@@ -71,7 +71,48 @@ namespace CalculatorApp
             }
             Console.WriteLine(numbers + "=" + sum);
         }
-            public static void Divide()
+
+        public static void Multiply()
+        {
+            string number;
+
+            string numTrim;
+
+            string[] numArray;
+
+            double num;
+
+            double sum;
+
+            List<double> numList = new List<double>();
+
+            Console.WriteLine("Please enter the two numbers you want multiplied: \n Example: X * X");
+
+            number = Console.ReadLine();
+
+            numArray = number.Split('*');
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                numTrim = numArray[i].Trim();
+
+                if (double.TryParse(numTrim, out num))
+                {
+                    numList.Add(num);
+                }
+            }
+
+            sum = numList[0];
+
+            for (int i = 1; i < numList.Count; i++)
+            {
+                sum = Calculator.Multiply(sum, numList[1]);
+            }
+
+            Console.WriteLine(number + "=" + sum);
+        }
+
+        public static void Divide()
         {
             string Numbers;
             string[] tmp;
@@ -103,46 +144,6 @@ namespace CalculatorApp
                     sum = CalculatorLibrary.Calculator.Divide(sum, nmb[k]);
                 }
                 Console.WriteLine("The numbers divide is: " + sum);
-            }
-
-            public static void Multiply()
-            {
-                string number;
-
-                string numTrim;
-
-                string[] numArray;
-
-                double num;
-
-                double sum;
-
-                List<double> numList = new List<double>();
-
-                Console.WriteLine("Please enter the two numbers you want multiplied: \n Example: X * X");
-
-                number = Console.ReadLine();
-
-                numArray = number.Split('*');
-
-                for (int i = 0; i < numArray.Length; i++)
-                {
-                    numTrim = numArray[i].Trim();
-
-                    if (double.TryParse(numTrim, out num))
-                    {
-                        numList.Add(num);
-                    }
-                }
-
-                sum = numList[0];
-
-                for (int i = 1; i < numList.Count; i++)
-                {
-                    sum = Calculator.Multiply(sum, numList[1]);
-                }
-
-                Console.WriteLine(number + "=" + sum);
             }
         }
 
