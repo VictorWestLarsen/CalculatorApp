1a1a4aa0c9d22a7ae0c6da168c7a99c9f28483d3
Christian T <chri466w@edu.eal.dk>
2018-10-09 14:33:51 +0200

    Implemented Multiply

--- CalculatorApp/MenuPoints.cs
+++ CalculatorApp/MenuPoints.cs
@@ -3,10 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalculatorLibrary;
 
 namespace CalculatorApp
 {
     public static class MenuPoints
     {
+        public static void Multiply()
+        {
+            string number;
+
+            string numTrim;
+
+            string[] numArray;
+
+            double num;
+
+            double sum;
+
+            List<double> numList = new List<double>();
+
+            Console.WriteLine("Please enter the two numbers you want multiplied: \n Example: X * X");
+
+            number = Console.ReadLine();
+
+            numArray = number.Split('*');
+
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                numTrim = numArray[i].Trim();
+
+                if (double.TryParse(numTrim, out num))
+                {
+                    numList.Add(num);
+                }
+            }
+
+            sum = numList[0];
+
+            for (int i = 1; i < numList.Count; i++)
+            {
+                sum = Calculator.Multiply(sum, numList[1]);
+            }
+
+            Console.WriteLine(number + "=" + sum);
+        }
     }
 }
