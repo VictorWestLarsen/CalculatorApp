e91600687f322cfb7c25eb69f64feda7031e4021
Christian T <chri466w@edu.eal.dk>
2018-10-09 14:35:34 +0200

    Implemented Multiply and Using CalcLib

--- CalculatorApp/MenuPoints.cs
+++ CalculatorApp/MenuPoints.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CalculatorLibrary;
 
 namespace CalculatorApp
 {
@@ -102,6 +103,46 @@ namespace CalculatorApp
                     sum = CalculatorLibrary.Calculator.Divide(sum, nmb[k]);
                 }
                 Console.WriteLine("The numbers divide is: " + sum);
+            }
+
+            public static void Multiply()
+            {
+                string number;
+
+                string numTrim;
+
+                string[] numArray;
+
+                double num;
+
+                double sum;
+
+                List<double> numList = new List<double>();
+
+                Console.WriteLine("Please enter the two numbers you want multiplied: \n Example: X * X");
+
+                number = Console.ReadLine();
+
+                numArray = number.Split('*');
+
+                for (int i = 0; i < numArray.Length; i++)
+                {
+                    numTrim = numArray[i].Trim();
+
+                    if (double.TryParse(numTrim, out num))
+                    {
+                        numList.Add(num);
+                    }
+                }
+
+                sum = numList[0];
+
+                for (int i = 1; i < numList.Count; i++)
+                {
+                    sum = Calculator.Multiply(sum, numList[1]);
+                }
+
+                Console.WriteLine(number + "=" + sum);
             }
         }
 
